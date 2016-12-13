79109d65cf785eb4fc5469da911367bd0db4bd90
unknown <sis@Lenovo-nb-04.ortmd.ort.md>
2016-12-13 20:24:13 +0200

    Removed unneded methods. Added get_all_params method. Modified .gitignore file.

--- lib/behave.py
+++ lib/behave.py
@@ -6,12 +6,17 @@ import ast
 
 class Behave():
 
-	def __init__(self, *args, **kwargs):		
+	def __init__(self, *args, **kwargs):
+
 		self.PATH = "data/user_typing_info/"
+
 		self.data = []
+
+
 		if (kwargs.get("path")):
 
 			self.PATH = kwargs.get("path")
+
 			self.data = self.read_from_file(self.PATH)
 
 
@@ -20,6 +25,7 @@ class Behave():
 			file_name = str(kwargs.get("file"))
 
 			self.data = self.read_from_file(self.PATH+file_name)
+
 
 
 		if (kwargs.get("data")):
@@ -103,6 +109,7 @@ class Behave():
 		"""
 		return sum(self.durations) / len(self.durations)
 
+
 	def space_delays(self):
 
 		tm_length = len(self.timestamps)
@@ -125,15 +132,41 @@ class Behave():
 		space_tm_average = sum(space_delays) / len(space_delays)
 
 		return space_tm_average
+
+
+
 	def average_difference(self, subject):
 		"""
 		/ subject = other test with comparision data.
 		Returns differences between subject and object tests.
 		"""
 		kps_diff = abs(self.keypress_per_second() - subject.keypress_per_second())
+
 		kdel_diff = abs(self.keypress_delay_average() - subject.keypress_delay_average())
+
 		kdur_diff = abs(self.keypress_duration_average() - subject.keypress_duration_average())
+
 		return {"kps_diff": kps_diff, "kdel_diff": kdel_diff, "kdur_diff": kdur_diff}
+
+
+	def get_all_params(self):
+		"""
+		Returns an array of all analyzed values as a dictionary
+		"""
+
+		val_arr = {}
+
+		val_arr['kps'] = self.keypress_per_second()
+
+		val_arr['kdel'] = self.keypress_delay_average()
+
+		val_arr['kdur'] = self.keypress_duration_average()
+
+
+		return val_arr
+
+
+
 	def compare_difference(self, subject):
 		"""
 		/ subject = other test with comparision data.
@@ -141,65 +174,22 @@ class Behave():
 		"""
 
 		# maximum differences for the same user
+
 		max_kps_diff = 0.06
+
 		max_kdel_diff = 10
+
 		max_kdur_diff = 10		
 	
+
 		difference = self.average_difference(subject)
+
 		kps_diff = difference["kps_diff"]
+
 		kdel_diff = difference["kdel_diff"]
+
 		kdur_diff = difference["kdur_diff"]
+
 		
 		return kps_diff < max_kps_diff and kdel_diff < max_kdel_diff and kdur_diff < max_kdur_diff
 				
-
-
-if __name__ == "__main__":
-
-
-	data = [{u'timestamp': 1480518416394, u'key_duration': 44, u'keycode': 83}, 
-	{u'timestamp': 1480518416452, u'key_duration': 53, u'keycode': 65}, 
-	{u'timestamp': 1480518416499, u'key_duration': 100, u'keycode': 78}, 
-	{u'timestamp': 1480518416870, u'key_duration': 36, u'keycode': 16}, 
-	{u'timestamp': 1480518416893, u'key_duration': 59, u'keycode': 36}, 
-	{u'timestamp': 1480518416956, u'key_duration': 122, u'keycode': 8}, 
-	{u'timestamp': 1480518417283, u'key_duration': 125, u'keycode': 17}, 
-	{u'timestamp': 1480518417778, u'key_duration': 94, u'keycode': 17}, 
-	{u'timestamp': 1480518417825, u'key_duration': 141, u'keycode': 17}, 
-	{u'timestamp': 1480518417825, u'key_duration': 141, u'keycode': 16}, 
-	{u'timestamp': 1480518417969, u'key_duration': 98, u'keycode': 76}, 
-	{u'timestamp': 1480518418166, u'key_duration': 112, u'keycode': 79}, 
-	{u'timestamp': 1480518418301, u'key_duration': 53, u'keycode': 82}, 
-	{u'timestamp': 1480518418379, u'key_duration': 131, u'keycode': 69}, 
-	{u'timestamp': 1480518418545, u'key_duration': 97, u'keycode': 77}, 
-	{u'timestamp': 1480518418571, u'key_duration': 123, u'keycode': 32}, 
-	{u'timestamp': 1480518418734, u'key_duration': 50, u'keycode': 73}, 
-	{u'timestamp': 1480518418783, u'key_duration': 99, u'keycode': 77}, 
-	{u'timestamp': 1480518419084, u'key_duration': 82, u'keycode': 8}, 
-	{u'timestamp': 1480518419399, u'key_duration': 99, u'keycode': 80}, 
-	{u'timestamp': 1480518419575, u'key_duration': 41, u'keycode': 83}, 
-	{u'timestamp': 1480518419617, u'key_duration': 83, u'keycode': 85}, 
-	{u'timestamp': 1480518419916, u'key_duration': 112, u'keycode': 32}, 
-	{u'timestamp': 1480518420151, u'key_duration': 78, u'keycode': 8}, 
-	{u'timestamp': 1480518420367, u'key_duration': 35, u'keycode': 77},
-	{u'timestamp': 1480518420400, u'key_duration': 0, u'keycode': 32}, 
-	{u'timestamp': 1480518420514, u'key_duration': 16, u'keycode': 68}, 
-	{u'timestamp': 1480518420583, u'key_duration': 85, u'keycode': 79}, 
-	{u'timestamp': 1480518420798, u'key_duration': 121, u'keycode': 76}, 
-	{u'timestamp': 1480518420983, u'key_duration': 107, u'keycode': 79}, 
-	{u'timestamp': 1480518421116, u'key_duration': 117, u'keycode': 32}, 
-	{u'timestamp': 1480518421335, u'key_duration': 0, u'keycode': 8}, 
-	{u'timestamp': 1480518421444, u'key_duration': 59, u'keycode': 82}, 
-	{u'timestamp': 1480518422487, u'key_duration': 53, u'keycode': 69}, 
-	{u'timestamp': 1480518422533, u'key_duration': 99, u'keycode': 84}, 
-	{u'timestamp': 1480518422828, u'key_duration': 61, u'keycode': 16}, 
-	{u'timestamp': 1480518422851, u'key_duration': 84, u'keycode': 49}]
-
-	behave = Behave(data)
-
-	print(behave.keypress_delay_deltas())
-	print(len(data))
-	print(keypress_delay_average(timestamps))
-	print(backspace_count(keycodes))
-	print(keypress_duration_average(durations))
-	print(space_delay_average(timestamps, keycodes))
