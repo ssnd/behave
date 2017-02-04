3d171202550e431d45f02ed4a461c78a4bca78cf
banana <banana@localhost.localdomain>
2017-02-04 14:00:47 +0200

    Added Neural Network-Testing System

--- tools/neural-recognition/main.py
+++ tools/neural-recognition/main.py
@@ -11,7 +11,7 @@ from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets.supervised import SupervisedDataSet
 from pybrain.tools.shortcuts import buildNetwork
 
-users = models.Collect.query.all()
+users = models.Collect.query.all()[15:15+10]
 user_count = len(users)
 
 training_data = []
@@ -24,11 +24,13 @@ for user_index in range(len(users)):
 
 	mouse_data_chunks = [user.mouseDataChunk1, user.mouseDataChunk2, user.mouseDataChunk3]
 
-	for data_chunk in keyboard_data_chunks:
+	for i in range(len(keyboard_data_chunks)):
 
-		instance = Keyboard(data=data_chunk)
+		keyboard_instance = Keyboard(data=keyboard_data_chunks[i])
 
-		params = instance.get_keyboard_params()
+		mouse_instance = Mouse(data=mouse_data_chunks[i])
+
+		params = dict(keyboard_instance.get_keyboard_params().items() + mouse_instance.get_mouse_params().items())
 
 		prepared_dict = {
 			"data" : params,
@@ -38,25 +40,13 @@ for user_index in range(len(users)):
 
 		training_data.append(prepared_dict)
 
-	for mouse_data_chunk in mouse_data_chunks:
-
-		instance = Mouse(data=mouse_data_chunk)
-
-		params = instance.get_mouse_params()
-
-		prepared_dict = {
-			"data" : params, 
-			"response" : user_index
-		}
-
-		training_data.append(prepared_dict)
-
 normalized_data = Behave.normalize_training_data(training_data)
+
 
 
 min_max = normalized_data['min_max']
 
-ds = SupervisedDataSet(4, user_count)
+ds = SupervisedDataSet(8, user_count)
 
 for index in range(len(normalized_data['responses'])):
 
@@ -64,10 +54,12 @@ for index in range(len(normalized_data['responses'])):
 
 	output = normalized_data['responses'][index]
 	# print (input, output)
+
+	# print "i: ", input, "...o: ", output
 	ds.addSample(input, output)
 
 
-net = buildNetwork(4,(3+user_count)*2,user_count)
+net = buildNetwork(8, 25,user_count)
 
 trainer = BackpropTrainer(net, learningrate = 0.01, momentum = 0.99)
 
@@ -76,23 +68,30 @@ trainer.trainOnDataset(ds, 1000)
 
 trainer.testOnData()
 
-print min_max
-
-
 # CHECKING NETWORK
 print "Checking network: "
-user = users[7]
+
+checking_id = 6;
+
+user = users[checking_id]
 
 print "User: ", user.id
 
-data = user.dataChunk4
+keyboard_test_data = user.dataChunk3
 
-instance = Keyboard(data=data)
+mouse_test_data = user.mouseDataChunk3
 
-print instance.get_keyboard_params()
+keyboard_test_instance = Keyboard(data=keyboard_test_data)
 
-nd = instance.normalize_data(min_max, instance.get_keyboard_params())
-print nd
+mouse_test_instance = Mouse(data=mouse_test_data)
+
+test_data_to_normalize = dict(keyboard_test_instance.get_keyboard_params().items() + mouse_test_instance.get_mouse_params().items())
+
+print test_data_to_normalize
+
+nd = keyboard_test_instance.normalize_data(min_max, test_data_to_normalize)
+
+#print "Normalized data: ", test_data_to_normalize, " / _ / ", nd
 
 print "Response: "
 
@@ -102,11 +101,15 @@ print response
 
 result = None
 
+maxResponse = 0
+
 for i in range(len(response)):
-	if response[i] > 0.5: result = i
+	if response[i] > maxResponse:
+		result = i
+		maxResponse = response[i]
 
-print result
+if maxResponse > 0.5: print "Guessed user is: ", users[result].id
+else: print "Guessed < 0.5 user is: ", users[result].id 
 
-guessed_user = users[result]
-
-print "Guessed user is: ", guessed_user.id
+if users[result].id == user.id: print "TRUE"
+else: print "FALSE"
